eee2c82ee43d1086856b8ff921091e87158e0ef1
yerndark <135379254+yerndark@users.noreply.github.com>
2023-11-16 03:27:38 +0600

    Update contact.js

--- src/classes/contact.js
+++ src/classes/contact.js
@@ -6,9 +6,9 @@ export default class {
     form () {
         return `
             <div>
-                <input placeholder="name" type="text" value="${this.email}" onchange="main.contact.email = this.value" />
-                <input placeholder="surname" type="text" value="${this.number}" onchange="main.contact.number = this.value"/>
-                <input placeholder="country" type="text" value="${this.address}" onchange="main.contact.address = this.value"/>
+                <input placeholder="email" type="text" value="${this.email}" onchange="main.contact.email = this.value" />
+                <input placeholder="number" type="text" value="${this.number}" onchange="main.contact.number = this.value"/>
+                <input placeholder="address" type="text" value="${this.address}" onchange="main.contact.address = this.value"/>
             </div>
         `
     }
